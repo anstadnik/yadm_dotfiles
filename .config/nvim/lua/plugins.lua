c64cc358ae0202238cc5fdcd31d5978860319a59
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-06-04 14:38:01 +0300

    Update bootstrap

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,6 +2,7 @@ vim.g.python3_host_prog = '/home/astadnik/.virtualenvs/neovim/bin/python3'
 
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  print("DOWNLOADING PACKER!")
   packer_bootstrap = vim.fn.system({ 'git', 'clone', '--depth', '1',
     'https://github.com/wbthomason/packer.nvim', install_path })
 end
