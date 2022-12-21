b4e61012b7110795c1a14a9ea11da8a404f13519
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-12-22 01:48:52 +0200

    Remove Linux files

--- .config/sway##os.Linux/scripts/light.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-CONTENT=$(curl -Ls https://freegeoip.app/json/)
-echo $CONTENT
-longitude=$(echo $CONTENT | jq .longitude)
-latitude=$(echo $CONTENT | jq .latitude)
-echo $longitude $latitude
-wlsunset -l $latitude -L $longitude
