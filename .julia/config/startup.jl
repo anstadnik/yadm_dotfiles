a743b17a5ce4be3f5d3b6835d1b792a491782800
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-02-15 12:24:41 +0200

    Updates

---
+++ .julia/config/startup.jl
@@ -0,0 +1,11 @@
+# import Combinatorics, Distributions, FameSVD,
+#     GLMakie, Intervals, Rotations,
+#     StatsBase,  Statistics, StructArrays
+#
+# using Debugger, LinearAlgebra, StaticArrays,
+#     TensorCast, Tullio, Test
+
+using Revise, Debugger
+# using Diploma
+using KittyTerminalImages
+forceKittyDisplay!()
