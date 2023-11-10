60b103016f8b20374ea4f85322f17f34e0658b0b
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-11-10 16:39:41 +0200

    Update abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,9 +10,50 @@ if status is-interactive
     export VISUAL=nvim
     export EDITOR="$VISUAL"
 
-    alias ls lsd
-    alias v=nvim
+    abbr -a ls lsd
+    abbr -a v nvim
 
+    switch (uname)
+        case Linux
+            abbr -a pacupg sudo pacman -Syu
+            abbr -a pacin sudo pacman -S
+            abbr -a paclean sudo pacman -Sc
+            abbr -a pacins sudo pacman -U
+            abbr -a paclr sudo pacman -Scc
+            abbr -a pacre sudo pacman -R
+            abbr -a pacrem sudo pacman -Rns
+            abbr -a pacrep pacman -Si
+            abbr -a pacreps pacman -Ss
+            abbr -a pacloc pacman -Qi
+            abbr -a paclocs pacman -Qs
+            abbr -a pacinsd sudo pacman -S --asdeps
+            abbr -a pacmir sudo pacman -Syy
+            abbr -a paclsorphans sudo pacman -Qdt
+            abbr -a pacrmorphans sudo pacman -Rs $(pacman -Qtdq)
+            abbr -a pacfileupg sudo pacman -Fy
+            abbr -a pacfiles pacman -F
+            abbr -a pacls pacman -Ql
+            abbr -a pacown pacman -Qo
+            abbr -a pacupd sudo pacman -Sy
+
+            abbr -a paclean paru -Sc
+            abbr -a paclr paru -Scc
+            abbr -a paupg paru -Syu
+            abbr -a pasu paru -Syu --noconfirm
+            abbr -a pain paru -S
+            abbr -a pains paru -U
+            abbr -a pare paru -R
+            abbr -a parem paru -Rns
+            abbr -a parep paru -Si
+            abbr -a pareps paru -Ss
+            abbr -a paloc paru -Qi
+            abbr -a palocs paru -Qs
+            abbr -a palst paru -Qe
+            abbr -a paorph paru -Qtd
+            abbr -a painsd paru -S --asdeps
+            abbr -a pamir paru -Syy
+            abbr -a paupd paru -Sy
+    end
 
     macchina
 end
