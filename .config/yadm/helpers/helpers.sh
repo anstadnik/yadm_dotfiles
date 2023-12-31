09552a4f796e540886257f4c1aea1333c64575da
Stadnik Andrii <stadnikandriy1@gmail.com>
2023-12-31 18:12:43 +0200

    Add helpers

---
+++ .config/yadm/helpers/helpers.sh
@@ -0,0 +1,14 @@
+command_exists(command) {
+  [[ -x $(command -v $command) ]]
+}
+
+run_in_background() {
+  if [[ command_exists gnome-shell && command_exists kitty ]]; then
+		kitty bash -c $@ &
+	else
+		$@ &
+	fi
+}
+
+
+# export -f install_gnome_extension_ubuntu install_theme_ubuntu install_icons_ubuntu install_fonts_ubuntu install_rustup_ubuntu
