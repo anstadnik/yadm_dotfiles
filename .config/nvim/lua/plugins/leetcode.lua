9f026ed3d927bd6724c3aaa6d492cbb8379b04c2
Stadnik Andrii <stadnikandriy1@gmail.com>
2024-01-13 17:46:30 +0200

    updates

---
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,37 @@
+local leet_arg = "leetcode.nvim"
+
+return {
+  {
+    "kawre/leetcode.nvim",
+    lazy = leet_arg ~= vim.fn.argv()[1],
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      -- "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+
+      lang = "rust",
+      arg = leet_arg,
+      hooks = {
+        ---@type fun(question: { lang: string })[]
+        LeetQuestionNew = {
+          function(_)
+            -- vim.keymap.set("n", "<CR>", "<cmd>Leet test<CR>", { buffer = true })
+            -- vim.keymap.set("n", "<leader><CR>", "<cmd>Leet submit<CR>", { buffer = true })
+
+            vim.keymap.set("n", "<CR>", "<cmd>w | Leet test<CR>", { buffer = true })
+            vim.keymap.set("n", "<leader><CR>", "<cmd>w | Leet submit<CR>", { buffer = true })
+          end,
+        },
+      },
+    },
+  },
+}
