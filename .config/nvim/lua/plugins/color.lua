8c2ce0b4b632b560115027a513b8cbc6d9e92d6d
Stadnik Andrii <stadnikandriy1@gmail.com>
2024-07-23 15:01:07 +0300

    updates

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -6,11 +6,11 @@ return {
     config = function()
       require("catppuccin").setup {
         transparent_background = true,
-        dim_inactive = {
-          enabled = true,
-          shade = "dark",
-          percentage = 0.15,
-        },
+        -- dim_inactive = {
+        --   enabled = true,
+        --   shade = "dark",
+        --   percentage = 0.15,
+        -- },
         integrations = {
           cmp = true,
           gitsigns = true,
