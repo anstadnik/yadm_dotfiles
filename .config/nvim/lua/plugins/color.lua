ab7dde544cb711bd567548c29b6f862bea339493
Stadnik Andrii <stadnikandriy1@gmail.com>
2024-11-01 22:02:34 +0200

    updates

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -47,7 +47,7 @@ return {
           -- -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       }
-      vim.opt.background = "light"
+      vim.opt.background = "dark"
       vim.cmd.colorscheme "catppuccin"
     end,
   }
